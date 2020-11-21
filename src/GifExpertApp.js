863e2b3994ce8ea896d1bfe22052a5832c7bd9ca
chriszdeux <chriszdeux@gmail.com>
2020-11-21 09:22:35 -0800

    build

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,19 +1,11 @@
 import React, { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
-// import PropTypes from 'prop-types';
 import { GifGrid } from './components/GifGrid';
 
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['']);
 
-  // const handleAdd = () => {
-  //   // setCategories( [...categories, 'Attack on Titans'] );
-  //   // setCategories( ['Dragon Ball', ...categories] )
-  //   // setCategories( cats => (
-  //   //   [...cats, 'Sword Art Online']
-  //   // ))
-  // }
   return (
     <>
       <h2>GifExpertApp</h2>
@@ -33,6 +25,3 @@ export const GifExpertApp = () => {
     </>
   )
 }
-
-
-export default GifExpertApp;
